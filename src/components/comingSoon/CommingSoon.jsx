c9fee06b4f7804b92c4c94d2006a3e2ef8152e1f
Faaiz Mahmood <faizzafar44@gmail.com>
2024-09-18 22:14:09 +0500

    add comming soon component

---
+++ src/components/comingSoon/CommingSoon.jsx
@@ -0,0 +1,14 @@
+import './CommingSoon.css'
+
+const CommingSoon = () => {
+    return (
+        <section id='comming-soon'>
+            <h1 className='main-heading'>Comming Soon</h1>
+            <p>Empowering education through innovation. SmartLearn is your go-to platform for interactive and personalized online learning,
+                designed to make education accessible anytime, anywhere. Stay tuned for a transformative digital learning experience tailored
+                to your needs.</p>
+        </section>
+    )
+}
+
+export default CommingSoon
---
+++ src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+
+
+export const LandingPage = () => {
+  return (
+    <>
+        <h1>HI</h1>
+    </>
+  )
+}
+
+
+export default LandingPage
