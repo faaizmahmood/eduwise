9bfceac5cf282d1edf33346cf5178979e4c81349
Faaiz Mahmood <faizzafar44@gmail.com>
2024-09-19 18:41:19 +0500

    fix deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  return(
   <>
   <Routes>
-    <Route path='/' element={<ComingSoon/>}/>
+    <Route exact path='/' element={<ComingSoon/>}/>
   </Routes>
     
   </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
