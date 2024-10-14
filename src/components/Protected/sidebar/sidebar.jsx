ab6cad607e7b398803f657dcd4ce6c0f400cb122
Faaiz Mahmood <faizzafar44@gmail.com>
2024-10-14 13:25:06 +0500

    demo purpose

--- src/components/Protected/sidebar/sidebar.jsx
+++ src/components/Protected/sidebar/sidebar.jsx
@@ -4,7 +4,11 @@ import styles from './sidebar.module.scss'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <>
+      <aside style={{ backgroundColor: '#FCEAEA', borderRadius: '16px', height:'100%' }} className='p-3'>
+        Sidebar
+      </aside>
+    </>
   )
 }
 
