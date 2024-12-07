c0abf8253ade67fc57b60bdc34877b6afe38dcea
Faaiz Mahmood <faizzafar44@gmail.com>
2024-12-07 15:14:26 +0500

    update config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +11,16 @@ export default defineConfig({
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser', // Fix Amplify config resolution
+      // Alias Node.js modules to browser-friendly versions
+      crypto: path.resolve('./node_modules/crypto-browserify'),
+      stream: path.resolve('./node_modules/stream-browserify'),
+      buffer: path.resolve('./node_modules/buffer'),
+      process: path.resolve('./node_modules/process/browser'),
     },
+  },
+  define: {
+    // Polyfill for `require`
+    'global': 'globalThis',
+    'process.env': {},
   },
 });
